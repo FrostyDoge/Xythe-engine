8f7c8faa0f5ce0a04f878bbff40afdafe1c34d3e
TheWolfLovers <thewolflovers12345@gmail.com>
2024-12-24 21:34:14 +0000

    Update Discord.hx

--- source/backend/Discord.hx
+++ source/backend/Discord.hx
@@ -98,7 +98,7 @@ class DiscordClient
 		isInitialized = true;
 	}
 
-	public static function changePresence(details:String = 'Eating a Burrito...', ?state:String, ?smallImageKey:String, ?hasStartTimestamp:Bool, ?endTimestamp:Float, largeImageKey:String = 'icon')
+	public static function changePresence(details:String = 'Eating a Burrito... (in menus)', ?state:String, ?smallImageKey:String, ?hasStartTimestamp:Bool, ?endTimestamp:Float, largeImageKey:String = 'icon')
 	{
 		var startTimestamp:Float = 0;
 		if (hasStartTimestamp) startTimestamp = Date.now().getTime();
